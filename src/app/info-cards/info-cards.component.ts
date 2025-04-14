bb908649afd99b44923b766609cc25b6d74d7143
elia-driesner-pdr <elia.driesner@pdr-team.de>
2025-04-14 08:50:52 +0200

    added icons to info cards

--- src/app/info-cards/info-cards.component.ts
+++ src/app/info-cards/info-cards.component.ts
@@ -13,17 +13,20 @@ export class InfoCardsComponent {
     {
       'title': 'Fragen beantworten', 
       'body': 'Allgemeine Fragen über PDR-Team und die Abläufe beantworten',
-      'example': 'Erzähle mir etwas über PDR Team'
+      'example': 'Erzähle mir etwas über PDR Team',
+      'icon': '<i class="bi bi-info-square-fill"></i>'
     },
     {
       'title': 'Daten abfragen', 
       'body': 'Welcher Station ist mein Kennzeichen [...] zugeordnet?',
-      'example': 'Welcher Station bin ich zugeordnet, mein Kennzeichen ist [Kennzeichen]'
+      'example': 'Welcher Station bin ich zugeordnet, mein Kennzeichen ist [Kennzeichen]',
+      'icon': '<i class="bi bi-bar-chart-fill"></i>'
     },
     {
       'title': 'Termine buchen', 
       'body': 'Einfach Besichtigungstermine ausmachen',
-      'example': 'Ich würde am [Datum] um [Uhrzeit] gerne ein Besichtigungstermin buchen, mein Kennzeichen ist [Kennzeichen]'
+      'example': 'Ich würde am [Datum] um [Uhrzeit] gerne ein Besichtigungstermin buchen, mein Kennzeichen ist [Kennzeichen]',
+      'icon': '<i class="bi bi-calendar3"></i>'
     },
   ];
 
